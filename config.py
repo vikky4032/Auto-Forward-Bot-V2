77797825544efc75e2cf3a9337e1dbdaf326cc0c
CPSharma4032 <168281435+CPSharma4032@users.noreply.github.com>
2025-03-07 14:05:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import environ 
 
 class Config:
-    API_ID = environ.get("API_ID", "577678")
-    API_HASH = environ.get("API_HASH", "d2c6e01uuiuiouioiuiou0fc6d7a1be")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "70955...") 
+    API_ID = environ.get("API_ID", "अपना_API_ID")
+    API_HASH = environ.get("API_HASH", "अपना_API_HASH")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "अपना_BOT_TOKEN") 
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
-    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://chhjgjkkjhkjhkjh@cluster0.xowzpr4.mongodb.net/")
+    DATABASE_URI = environ.get("DATABASE", "अपना_MongoDB_URI")
     DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6964148334').split()]
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", 'अपना_Telegram_User_ID').split()]
 
 class temp(object): 
     lock = {}
@@ -15,4 +15,3 @@ class temp(object):
     forwardings = 0
     BANNED_USERS = []
     IS_FRWD_CHAT = []
-    
