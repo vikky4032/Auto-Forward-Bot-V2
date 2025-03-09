0ea1b9b19157782bc9e733e3b1450ff5137a459f
vikky4032 <1602609@gmail.com>
2025-03-09 16:50:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+# Install dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Download and install external ZIP file
+wget -O gagantelethon.zip https://devgagan.in/wp-content/uploads/2023/12/gagantelethon.zip
+unzip gagantelethon.zip
+cd gagantelethon || exit
+pip install .
+
+# Go back to main directory
+cd ..
+
+# Start the bot
+python3 main.py &
+
+# Keep instance alive
+tail -f /dev/null
